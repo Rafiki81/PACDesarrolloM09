7d4136cf04e3a4125083dde67b23aebcb1035c6d
Rafael Perez-Beato <rperezbeato@gmail.com>
2020-05-26 09:09:45 +0200

    Se mantiene el servidor Abierto a conexiones.

--- src/server/Server.java
+++ src/server/Server.java
@@ -20,35 +20,40 @@ public class Server {
     }
 
     public void initServer() throws IOException {
-        System.out.println("Esperando la conexion de un Cliente...");
-        socket = serverSocket.accept();
-        System.out.println("Cliente conectado.");
-        DataOutputStream salidaServidor = new DataOutputStream(socket.getOutputStream());
-        DataInputStream entradaServidor = new DataInputStream(socket.getInputStream());
-        String numeroTareas;
-        int numero;
-        String nombre = "";
-        salidaServidor.writeUTF("Hola, ¿Como te llamas?");
-        nombre = entradaServidor.readUTF();
-        salidaServidor.writeUTF("¿Cuantas Tareas hay que realizar?");
-        numeroTareas = entradaServidor.readUTF();
-        numero = Integer.parseInt(numeroTareas);
-        ArrayList<Tarea> tareas = new ArrayList<>();
-        for (int i = 1; i <= numero; i++) {
-            Tarea tarea = new Tarea();
-            salidaServidor.writeUTF("Tarea numero " + i);
-            salidaServidor.writeUTF("Introduce la descripcion de la Tarea");
-            tarea.setDescripcion(entradaServidor.readUTF());
-            salidaServidor.writeUTF("Introduce el estado de la Tarea");
-            tarea.setEstado(entradaServidor.readUTF());
-            tareas.add(tarea);
+
+        while(true){
+            System.out.println("Esperando la conexion de un Cliente...");
+            socket = serverSocket.accept();
+            System.out.println("Cliente conectado.");
+            DataOutputStream salidaServidor = new DataOutputStream(socket.getOutputStream());
+            DataInputStream entradaServidor = new DataInputStream(socket.getInputStream());
+            String numeroTareas;
+            int numero;
+            String nombre = "";
+            salidaServidor.writeUTF("Hola, ¿Como te llamas?");
+            nombre = entradaServidor.readUTF();
+            salidaServidor.writeUTF("¿Cuantas Tareas hay que realizar?");
+            numeroTareas = entradaServidor.readUTF();
+            numero = Integer.parseInt(numeroTareas);
+            ArrayList<Tarea> tareas = new ArrayList<>();
+            for (int i = 1; i <= numero; i++) {
+                Tarea tarea = new Tarea();
+                salidaServidor.writeUTF("Tarea numero " + i);
+                salidaServidor.writeUTF("Introduce la descripcion de la Tarea");
+                tarea.setDescripcion(entradaServidor.readUTF());
+                salidaServidor.writeUTF("Introduce el estado de la Tarea");
+                tarea.setEstado(entradaServidor.readUTF());
+                tareas.add(tarea);
+            }
+            salidaServidor.writeUTF("Las tareas de " + nombre + " son:");
+            for(Tarea tarea:tareas){
+                salidaServidor.writeUTF(tarea.toString());
+                salidaServidor.writeUTF("\n");
+            }
+            salidaServidor.writeUTF("Se finaliza la Conexión");
+
         }
-        salidaServidor.writeUTF("Las tareas de " + nombre + " son:");
-        for(Tarea tarea:tareas){
-            salidaServidor.writeUTF(tarea.toString());
-            salidaServidor.writeUTF("\n");
-        }
-        salidaServidor.writeUTF("Se finaliza la Conexión");
+
     }
 
 }
